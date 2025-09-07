35180f737cdc1eb2ba26d3729bd89df78c51ca31
gnaservicesinc <alast.andy@gmail.com>
2025-09-06 21:21:58 -0400

    init

---
+++ FileScap/FileScap/FileScapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileScapApp.swift
+//  FileScap
+//
+//  Created by Andrew Smith on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct FileScapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
